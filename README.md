ba6bbee57c972539145afcb0345a358fdc4f815f
Daniel Verkamp <daniel@drv.nu>
2012-12-06 20:09:07 -0700

    Hook up SDL and OpenGL dependencies

--- README.md
+++ README.md
@@ -3,6 +3,22 @@ Narfblock
 
 # Building
 
+## Prerequisites
+
+- CMake 2.6 or newer
+- libSDL 1.2 development libraries
+
+## Build Commands
+
+First get a copy of the source:
+
 	git clone https://github.com/narfblock/narfblock.git
 	cd narfblock/build
+
+Then generate a Makefile using CMake and perform the build:
+
 	cmake ../src && make
+
+Or on Windows with MinGW in an MSYS shell:
+
+	cmake -G "MSYS Makefiles" ../src && make
--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,25 @@
+#include <SDL/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+#include <stdlib.h>
 #include <stdio.h>
 #include "test.h"
 
-int main() {
-  printf("Version: %d.%d%s\n", VERSION_MAJOR, VERSION_MINOR, VERSION_RELEASE);
-  return 0;
+extern "C" int main(int argc, char **argv)
+{
+	printf("Version: %d.%d%s\n", VERSION_MAJOR, VERSION_MINOR, VERSION_RELEASE);
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL_Init(SDL_INIT_VIDEO) failed: %s\n", SDL_GetError());
+		SDL_Quit();
+		return 1;
+	}
+
+	const SDL_VideoInfo *vid_info = SDL_GetVideoInfo();
+
+	printf("Current video mode is %dx%dx%d\n", vid_info->current_w, vid_info->current_h, vid_info->vfmt->BitsPerPixel);
+
+	SDL_Quit();
+	return 0;
 }
