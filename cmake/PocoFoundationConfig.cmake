92a1472e714e13d3cad46b9e451d66ed832d6204
Jessica Creighton <jess@femtobit.org>
2013-08-06 22:35:32 -0400

    Readded Boost to cmake until we get rid of yamlcpp. Added some patching lines in cmake for libpoco to hopefully work on Windows better

--- cmake/PocoFoundationConfig.cmake
+++ cmake/PocoFoundationConfig.cmake
@@ -2,11 +2,11 @@ if(NOT DEFINED PocoFoundation_FOUND)
 
 	set(POCO_FOUNDATION_LIBRARY libPocoFoundation.a)
 	find_path(POCO_FOUNDATION_INCLUDE_DIR Poco/Foundation.h PATH "${PocoFoundation_DIR}/include")
-	# Error.cpp doesn't build on linux, but isn't needed by anything either.
-	# So let's just blank the file so it'll compile.
-	execute_process(COMMAND ${CMAKE_COMMAND} -E remove -f "${PocoFoundation_DIR}/src/Error.cpp")
-	execute_process(COMMAND ${CMAKE_COMMAND} -E touch "${PocoFoundation_DIR}/src/Error.cpp")
+	execute_process(COMMAND ${CMAKE_COMMAND} -E echo "Meow: ${PATCH_COMMAND}")
+	execute_process(COMMAND ${PATCH_COMMAND} --merge "../poco/Foundation/CMakeLists.txt" "../cmake/PocoFoundation.CMakeLists.patch")
 	include_directories("${POCO_FOUNDATION_INCLUDE_DIR}")
+	add_definitions( -DPOCO_STATIC -DPOCO_NO_AUTOMATIC_LIBS)
+	set( LIB_MODE STATIC )
 
 	add_subdirectory(
 		${PocoFoundation_DIR}
