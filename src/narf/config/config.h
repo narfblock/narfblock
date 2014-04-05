508d811737562f1c917777dc63e42fef74cea9f5
Daniel Verkamp <daniel@drv.nu>
2014-04-05 16:05:32 -0700

    Reformat config.h and mark overrides

--- src/narf/config/config.h
+++ src/narf/config/config.h
@@ -13,33 +13,31 @@ namespace narf {
 	namespace config {
 
 		class ConfigIni : public Poco::Util::IniFileConfiguration {
-			public:
-				ConfigIni() : Poco::Util::IniFileConfiguration() {};
-				ConfigIni(std::string& file) : Poco::Util::IniFileConfiguration(file) {};
-				bool getRaw(const std::string& key, std::string& value) const {
-					return Poco::Util::IniFileConfiguration::getRaw(key, value);
-				}
-				void setRaw(const std::string& key, const std::string& value) {
-					return Poco::Util::IniFileConfiguration::setRaw(key, value);
-				}
-				void enumerate(const std::string& key, Keys& range) const {
-					IniFileConfiguration::enumerate(key, range);
-				}
-
+		public:
+			ConfigIni() : Poco::Util::IniFileConfiguration() {};
+			ConfigIni(std::string& file) : Poco::Util::IniFileConfiguration(file) {};
+			bool getRaw(const std::string& key, std::string& value) const {
+				return Poco::Util::IniFileConfiguration::getRaw(key, value);
+			}
+			void setRaw(const std::string& key, const std::string& value) {
+				return Poco::Util::IniFileConfiguration::setRaw(key, value);
+			}
+			void enumerate(const std::string& key, Keys& range) const {
+				IniFileConfiguration::enumerate(key, range);
+			}
 		};
 
 		class ConfigManager : public Poco::Util::AbstractConfiguration {
-			public:
-				ConfigManager() {};
-				void load(std::string name, std::string filename);
-				bool getRaw(const std::string& key, std::string& value) const;
-				void setRaw(const std::string& key, const std::string& value);
-				void enumerate(const std::string& key, Keys& range) const;
-				Poco::NotificationCenter notificationCenter;
-			private:
-				mutable std::map<const std::string, ConfigIni*> configs;
+		public:
+			ConfigManager() {};
+			void load(std::string name, std::string filename);
+			bool getRaw(const std::string& key, std::string& value) const override;
+			void setRaw(const std::string& key, const std::string& value) override;
+			void enumerate(const std::string& key, Keys& range) const;
+			Poco::NotificationCenter notificationCenter;
+		private:
+			mutable std::map<const std::string, ConfigIni*> configs;
 		};
-
 	}
 }
 
