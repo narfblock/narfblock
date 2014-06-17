112fb57079a3308303a464d7b6d9812993493510
Daniel Verkamp <daniel@drv.nu>
2014-06-17 17:53:42 +0000

    freetype-gl: remove unused files

--- cmake/Modules/Findfreetype-gl.cmake
+++ cmake/Modules/Findfreetype-gl.cmake
@@ -1,16 +1,10 @@
 set (FREETYPE_GL_SRC
   freetype-gl.h      vec234.h
-  markup.h
   texture-atlas.c    texture-atlas.h
   texture-font.c     texture-font.h
-  font-manager.c     font-manager.h
   vector.c           vector.h
   platform.c         platform.h
-  opengl.h
-  mat4.c             mat4.h
-  shader.c           shader.h
-  vertex-buffer.c    vertex-buffer.h
-  vertex-attribute.c vertex-attribute.h)
+  opengl.h)
 
 
 set(FREETYPE_GL_SRC_LIST ${FREETYPE_GL_SRC})
