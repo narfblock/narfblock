89d8f5397f45a408f686150000e7111bc9e7b981
Daniel Verkamp <daniel@drv.nu>
2015-02-24 00:17:07 -0700

    Adjust FindGLEW.cmake for use outside cmake

--- cmake/Modules/FindGLEW.cmake
+++ cmake/Modules/FindGLEW.cmake
@@ -46,7 +46,7 @@ find_library(GLEW_LIBRARY NAMES GLEW glew32 glew glew32s PATH_SUFFIXES lib64)
 set(GLEW_INCLUDE_DIRS ${GLEW_INCLUDE_DIR})
 set(GLEW_LIBRARIES ${GLEW_LIBRARY})
 
-include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
+include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GLEW
                                   REQUIRED_VARS GLEW_INCLUDE_DIR GLEW_LIBRARY)
 
