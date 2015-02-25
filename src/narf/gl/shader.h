ca7da854ea154d27aa14899adae2ec17e8bff3b8
Daniel Verkamp <daniel@drv.nu>
2015-02-24 19:56:36 -0700

    Remove unused GL shader enumeration items
    They aren't needed for the potential shaders in NarfBlock and aren't
    compatible with old GL headers.

--- src/narf/gl/shader.h
+++ src/narf/gl/shader.h
@@ -13,10 +13,7 @@ class Context; // forward decl
 
 
 enum class ShaderType {
-	Compute = GL_COMPUTE_SHADER,
 	Vertex = GL_VERTEX_SHADER,
-	TessControl = GL_TESS_CONTROL_SHADER,
-	TessEvaluation = GL_TESS_EVALUATION_SHADER,
 	Geometry = GL_GEOMETRY_SHADER,
 	Fragment = GL_FRAGMENT_SHADER,
 };
