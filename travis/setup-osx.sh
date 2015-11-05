ffa287c66c91b347ec77b01fa821f42c8f56d2bf
Daniel Verkamp <daniel@drv.nu>
2015-11-05 00:05:02 -0700

    travis: attempt to compile opus libs on OS X

--- travis/setup-osx.sh
+++ travis/setup-osx.sh
@@ -2,10 +2,32 @@
 
 set -xe
 
+# already installed/provided by system:
+# - cmake
+# - zlib
+# - libpng
+
 brew install \
-    cmake \
     sdl2 \
-    libpng \
     freetype \
     enet \
     libogg
+
+# not currently provided by homebrew:
+# - libopus
+# - opusfile
+
+wget http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz
+tar zxf opus-1.1.tar.gz
+(cd opus-1.1
+    ./configure
+    make
+    make install
+)
+
+wget http://downloads.xiph.org/releases/opus/opusfile-0.6.tar.gz
+(cd opusfile-0.6
+    ./configure
+    make
+    make install
+)
