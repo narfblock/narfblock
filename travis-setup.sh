35790228868325fd77df23a34ca792d2f59ce4c6
Daniel Verkamp <daniel@drv.nu>
2015-11-04 23:49:19 -0700

    travis: don't run brew under sudo
    Move the sudo invocations back into the Linux-specific commands.
    
    brew complains when run as root, so don't use sudo for that.

--- travis-setup.sh
+++ travis-setup.sh
@@ -6,12 +6,12 @@ os=$(uname)
 
 case "$os" in
 Linux)
-    add-apt-repository ppa:zoogie/sdl2-snapshots -y
-    add-apt-repository ppa:ubuntu-toolchain-r/test -y
-    add-apt-repository ppa:jblgf0/multimedia-precise -y
-    apt-get update -qq
+    sudo add-apt-repository ppa:zoogie/sdl2-snapshots -y
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+    sudo add-apt-repository ppa:jblgf0/multimedia-precise -y
+    sudo apt-get update -qq
 
-    apt-get install -y \
+    sudo apt-get install -y \
         cmake \
         libsdl2-dev \
         libpng12-dev \
@@ -25,13 +25,13 @@ Linux)
         libgtest-dev \
         g++-4.8
 
-    update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 
     # libgtest-dev just installs the source, so build it and install it system-wide
     cd /usr/src/gtest
-    cmake -DBUILD_SHARED_LIBS:BOOL=ON .
-    cmake --build .
-    mv libg*.so /usr/local/lib/
+    sudo cmake -DBUILD_SHARED_LIBS:BOOL=ON .
+    sudo cmake --build .
+    sudo mv libg*.so /usr/local/lib/
     ;;
 
 Darwin)
