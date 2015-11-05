be5fd1d26a2f10b135de8fc628096500586f25e2
Daniel Verkamp <daniel@drv.nu>
2015-11-05 00:19:17 -0700

    travis: install gtest on OS X

--- travis/setup-osx.sh
+++ travis/setup-osx.sh
@@ -16,6 +16,7 @@ brew install \
 # not currently provided by homebrew:
 # - libopus
 # - opusfile
+# - gtest
 
 wget http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz
 tar zxf opus-1.1.tar.gz
@@ -31,4 +32,14 @@ tar zxf opusfile-0.6.tar.gz
     ./configure
     make
     make install
+)
+
+wget https://github.com/google/googletest/archive/release-1.7.0.tar.gz -O gtest.tar.gz
+tar zxf gtest.tar.gz
+(cd gtest-*
+    c++ -isystem include -I. -pthread -c src/gtest-all.cc
+    c++ -isystem include -I. -pthread -c src/gtest_main.cc
+    ar -rv /usr/local/lib/libgtest.a gtest-all.o
+    ar -rv /usr/local/lib/libgtest_main.a gtest_main.o
+    cp -R include/gtest /usr/local/include
 )
